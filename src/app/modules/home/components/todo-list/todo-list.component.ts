fe38692284f5fe9f9144f0b3ed084de67c976ded
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-08-28 11:41:27 -0300

    delete items actions added

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -9,9 +9,20 @@ import { TaskListItem } from '../../model/task-list-item'
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
-  public tasksList: TaskListItem[] = []
+  public tasksList: TaskListItem[] = [{ checked: false, name: 'test 1' }]
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  public listItemDelete(index: number) {
+    this.tasksList.splice(index, 1)
+  }
+
+  public listItemsDeleteAll() {
+    const confirm = window.confirm(
+      'Deseja realmente deletar todos os itens da lista?',
+    )
+    if (confirm) this.tasksList = []
+  }
 }
